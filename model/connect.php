1e896c81d08d5a630407ce5c909c89e7f570ed6a
Ngo Quang Duong <96757935+DuongNQ09@users.noreply.github.com>
2024-05-10 09:18:14 +0000

    1

--- model/connect.php
+++ model/connect.php
@@ -2,28 +2,30 @@
 
 class Database
 {
-	public $conn = NULL;
-	private $server = 'localhost';
-	private $dbName = 'quanlysach';
-	private $user = 'root';
-	private $password = '';
+    public $conn = NULL;
+    private $server = 'localhost';
+    private $dbName = 'quanlysach';
+    private $user = 'root';
+    private $password = '';
         
-        // Hàm kết nối CSDL
-	public function connect()
-	{
-		$conn = new mysqli($server, $user, $password, $dbName);
+    // Hàm kết nối CSDL
+    public function connect()
+    {
+        $this->conn = new mysqli($this->server, $this->user, $this->password, $this->dbName);
 
-		if ($conn->connect_error) {
-			printf($conn->connect_error);
-			exit();
-		}
-		$conn->set_charset("utf8");
-	}
-        // Hàm đóng kết nối CSDL
-        public function closeDatabase()
-	{
-		if ($conn) {
-			$conn->close();
-		}
-	}
+        if ($this->conn->connect_error) {
+            printf($this->conn->connect_error);
+            exit();
+        }
+        $this->conn->set_charset("utf8");
+    }
+    
+    // Hàm đóng kết nối CSDL
+    public function closeDatabase()
+    {
+        if ($this->conn) {
+            $this->conn->close();
+        }
+    }
 }
+?>
